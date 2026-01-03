4d0cdb60d64cadf8cc0fc438d6dfaa7eb6ce1109
realcatapult <andyx3418@gmail.com>
2026-01-02 23:04:30 -0800

    v1 test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:mapapp/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
